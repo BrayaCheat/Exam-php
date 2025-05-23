df0e718ceb1453443ab99c8e2fac92d972510762
BrayaVTech <chanchaobraya@vtech.one>
2025-05-23 14:16:47 +0700

    refactor(mock): upload multiple files

--- 30/index.php
+++ 30/index.php
@@ -1,32 +1,35 @@
 <html lang="en">
+
 <head>
   <title>30. Search by name & dob</title>
 </head>
+
 <body>
   <?php
-    $message = "";
-    if(
-      $_SERVER['REQUEST_METHOD'] === 'POST' &&
-      isset($_POST['name']) &&
-      isset($_POST['dob'])
-    ){
-      $client = $pdo->prepare("SELECT * FROM student WHERE name = :name AND dob = :dob");
-      $client->execute([
-        ":name" => htmlspecialchars($_POST['name']),
-        ":dob" => htmlspecialchars($_POST["dob"])
-      ]);
-      $student = $client->fetch();
-      if($student){
-        $message = "Found!";
-      }else {
-        $message = "Not found!";
-      }
+  $message = "";
+  if (
+    $_SERVER['REQUEST_METHOD'] === 'POST' &&
+    isset($_POST['name']) &&
+    isset($_POST['dob'])
+  ) {
+    $client = $pdo->prepare("SELECT * FROM student WHERE name = :name AND dob = :dob");
+    $client->execute([
+      ":name" => htmlspecialchars($_POST['name']),
+      ":dob" => htmlspecialchars($_POST["dob"])
+    ]);
+    $student = $client->fetch();
+    if ($student) {
+      $message = "Found!";
+    } else {
+      $message = "Not found!";
     }
+  }
   ?>
 
   <div>
     Student
-    <?= $message?>
+    <?= $message ?>
   </div>
 </body>
+
 </html>
---
+++ mock/index.php
@@ -0,0 +1,43 @@
+<?php
+  session_start();
+?>
+
+<html lang="en">
+<head>
+  <title>Mock Exam</title>
+</head>
+<body>
+  <?php
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+      $files = $_FILES['file'];
+      // $ext = pathinfo($file['name'], PATHINFO_EXTENSION);
+      $path = 'uploads';
+      // $maxSize = 2 * 1024 * 1024;
+      // $allowed = ['jpg', 'jpeg', 'png'];
+
+      // if(!in_array(strtolower($ext), $allowed)) {
+      //   echo "Invalid file extension!";
+      //   return;
+      // }
+
+      // if($file['size'] > $maxSize) {
+      //   echo "File must below 2MB!";
+      //   return;
+      // }
+
+      if(!is_dir($path)){
+        mkdir($path, 0755, true);
+      }
+
+      foreach ($files['tmp_name'] as $i => $tmp){
+        move_uploaded_file($tmp, $path . '/' . $files['name'][$i]);
+      }
+    }
+  ?>
+  <form method="post" enctype="multipart/form-data">
+    <input type="file" name="file[]" multiple required>
+    <button type="submit">upload</button>
+  </form>
+</body>
+</html>
